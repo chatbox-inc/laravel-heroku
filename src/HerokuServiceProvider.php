ca394cc985f8c84a74b584c683abca5b2f1af51d
mikakane <mikakane2@gmail.com>
2016-02-05 06:09:27 +0900

    change_loghandler

--- src/HerokuServiceProvider.php
+++ src/HerokuServiceProvider.php
@@ -23,7 +23,7 @@ class HerokuServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->extend(LoggerInterface::class,function(Logger $log){
-            $handler = new StreamHandler('php://stderr', Logger::WARNING);
+            $handler = new StreamHandler('php://stdout', Logger::WARNING);
             $handler->setFormatter(new LineFormatter("%message%\n", null, true, true));
             $log->setHandlers([$handler]);
             return $log;
